9bc9428e6cdf1f98e5e4c8c42faad9889d43fa71
John Gabriele <jmg3000@gmail.com>
2012-04-19 03:50:02 -0400

    Removed print statements from build-html.py to silence cron. Also added flag to rebuild all html.

--- docs/build-html.py
+++ docs/build-html.py
@@ -7,12 +7,14 @@ pandoc_cmd2 = '''pandoc -s          --css=styles.css -H _head-inc -B _nav-header
 
 txt_files = glob.glob('*.txt')
 
+DO_THEM_ALL = True
+
 for txt_fn in txt_files:
     html_fn = txt_fn[:-3] + 'html'
-    if not os.path.exists(html_fn) or os.path.getmtime(txt_fn) > os.path.getmtime(html_fn):
+    if DO_THEM_ALL or (not os.path.exists(html_fn)) or os.path.getmtime(txt_fn) > os.path.getmtime(html_fn):
         if txt_fn == 'index.txt' or txt_fn == 'appendix-a-index.txt' or txt_fn == 'appendix-g-glossary.txt':
-            print "Processing:", txt_fn, '-->', html_fn
+            #print "Processing:", txt_fn, '-->', html_fn
             os.system(pandoc_cmd2 % (html_fn, txt_fn))
         else:
-            print "Processing:", txt_fn, '-->', html_fn
+            #print "Processing:", txt_fn, '-->', html_fn
             os.system(pandoc_cmd % (html_fn, txt_fn))
