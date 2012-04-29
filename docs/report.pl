f137f81feb1e602585e25bcb016eb4c9ca0c054e
Herbert Breunung <lichtkind@cpan.org>
2012-04-29 18:18:09 +0200

    report now also includes anchors and links and otehr appendices

--- docs/report.pl
+++ docs/report.pl
@@ -1,10 +1,24 @@
 open my $FH, '<', "appendix-a-index.txt";
 my $c = do {local $/; <$FH>}; 
-my @d =  ($c =~ /\n\n<a id/g); 
-print "Index A items:",scalar @d, "\n";
+my @items =  ($c =~ /\n\n<a id/g); 
+my @anchor =  ($c =~ /<a id="/g); 
+my @links =  ($c =~ /\]\(/g); 
+print "Index A items:",scalar @items, " anchor:",scalar @anchor, " links:", scalar @links, "\n";
+
+open  $FH, '<', "appendix-b-grouped.txt";
+$c = do {local $/; <$FH>}; 
+@links =  ($c =~ /\]\(/g); 
+print "Index B                     links:", scalar @links, "\n";
+
+open  $FH, '<', "appendix-d-delta.txt";
+$c = do {local $/; <$FH>}; 
+@links =  ($c =~ /\]\(/g); 
+print "Index D                     links:", scalar @links, "\n";
 
 open  $FH, '<', "appendix-g-glossary.txt";
 $c = do {local $/; <$FH>}; 
-@d =  ($c =~ /\n\n<a id/g); 
-print "Index G items:",scalar @d, "\n";
+@items =  ($c =~ /\n\n<a id/g); 
+@anchor =  ($c =~ /<a id="/g); 
+@links =  ($c =~ /\]\(/g); 
+print "Index G items:",scalar @items, " anchor:",scalar @anchor, " links:", scalar @links, "\n";
 
