203dfa88d2df6f3ba42bf852eab9fe3f72d8e834
Herbert Breunung <lichtkind@cpan.org>
2012-04-29 23:23:49 +0200

    delete Seq, its still insome signatures but spec has to lead the way

--- docs/report.pl
+++ docs/report.pl
@@ -8,17 +8,17 @@ print "Index A items:",scalar @items, " anchor:",scalar @anchor, " links:", scal
 open  $FH, '<', "appendix-b-grouped.txt";
 $c = do {local $/; <$FH>}; 
 @links =  ($c =~ /\]\(/g); 
-print "Index B                     links:", scalar @links, "\n";
+print "Index B                      links:", scalar @links, "\n";
 
 open  $FH, '<', "appendix-d-delta.txt";
 $c = do {local $/; <$FH>}; 
 @links =  ($c =~ /\]\(/g); 
-print "Index D                     links:", scalar @links, "\n";
+print "Index D                      links:", scalar @links, "\n";
 
 open  $FH, '<', "appendix-g-glossary.txt";
 $c = do {local $/; <$FH>}; 
 @items =  ($c =~ /\n\n<a id/g); 
 @anchor =  ($c =~ /<a id="/g); 
 @links =  ($c =~ /\]\(/g); 
-print "Index G items:",scalar @items, " anchor:",scalar @anchor, " links:", scalar @links, "\n";
+print "Index G  items:",scalar @items, "  anchor:",scalar @anchor, " links:", scalar @links, "\n";
 
