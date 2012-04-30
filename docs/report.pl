efe012c2efa0ed88b6e67c882b9984021d0070e7
Herbert Breunung <lichtkind@cpan.org>
2012-04-30 18:48:21 +0200

    reporter script using now prinf and it looks just better

--- docs/report.pl
+++ docs/report.pl
@@ -3,22 +3,22 @@ my $c = do {local $/; <$FH>};
 my @items =  ($c =~ /\n\n<a id/g); 
 my @anchor =  ($c =~ /<a id="/g); 
 my @links =  ($c =~ /\]\(/g); 
-print "Index A items:",scalar @items, " anchor:",scalar @anchor, " links:", scalar @links, "\n";
+printf "Index A  items: %4d  anchor: %4d  links: %4d\n", int @items, int @anchor, int @links;
 
 open  $FH, '<', "appendix-b-grouped.txt";
 $c = do {local $/; <$FH>}; 
 @links =  ($c =~ /\]\(/g); 
-print "Index B                      links:", scalar @links, "\n";
+printf "Index B                             links: %4d\n", int @links;
 
 open  $FH, '<', "appendix-d-delta.txt";
 $c = do {local $/; <$FH>}; 
 @links =  ($c =~ /\]\(/g); 
-print "Index D                      links:", scalar @links, "\n";
+printf "Index D                             links: %4d\n", int @links;
 
 open  $FH, '<', "appendix-g-glossary.txt";
 $c = do {local $/; <$FH>}; 
 @items =  ($c =~ /\n\n<a id/g); 
 @anchor =  ($c =~ /<a id="/g); 
 @links =  ($c =~ /\]\(/g); 
-print "Index G  items:",scalar @items, "  anchor:",scalar @anchor, " links:", scalar @links, "\n";
+printf "Index G  items: %4d  anchor: %4d  links: %4d\n", int @items, int @anchor, int @links;
 
