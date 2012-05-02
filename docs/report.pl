2c73ee9c1daf2d582711994d91e424d36caef7c2
Herbert Breunung <lichtkind@cpan.org>
2012-05-02 02:18:06 +0200

    explaining: term, expression and statement

--- docs/report.pl
+++ docs/report.pl
@@ -3,19 +3,21 @@ use warnings;
 use v5.10;
 
 my (%file, %data, %sum, $FH, $content, @fn_parts);
+my %pattern = (
+	item   => qr/\n\n<a id/,
+	anchor => qr/<a id="/,
+	link   => qr/\]\(/,
+	line   => qr/\n/,
+);
+
+# filter out all the tablet files
 for my $md_file (<*.txt>){
 	@fn_parts = split '-', $md_file;
 	next unless @fn_parts > 1;
 	$file{ ucfirst ($fn_parts[0]) . '-' . uc($fn_parts[1]) } = $md_file;
 }
 
-my %pattern = (
-	item   => qr/\n\n<a id/,
-	anchor => qr/<a id="/,
-	'link' => qr/\]\(/,
-);
-
-
+# and sniff through them
 for my $page (sort keys %file) {
 	open $FH, '<', $file{$page};
 	$content = do {local $/; <$FH>};
@@ -25,10 +27,10 @@ for my $page (sort keys %file) {
 		$sum{$type} += $data{$type};
 	}
 	$sum{'size'} += $data{'size'};
-	printf "%12s  items: %4d  anchor: %4d  links: %4d  bytes: %6.d\n", 
-		$page, $data{'item'}, $data{'anchor'}, $data{'link'}, $data{'size'};
+	printf "%11s  items: %4d  anchor: %4d  links: %5d  line: %5d  bytes: %6.d\n", 
+		$page, $data{'item'}, $data{'anchor'}, $data{'link'}, $data{'line'}, $data{'size'};
 }
-print '-' x 67,"\n";
-printf "Sum           items: %4d  anchor: %4d  links: %4d  bytes: %6d\n",
-	$sum{'item'}, $sum{'anchor'}, $sum{'link'}, $sum{'size'};
+print '-' x 80,"\n";
+printf "Sum          items: %4d  anchor: %4d  links: %5d  line: %5d  bytes: %6d\n",
+	$sum{'item'}, $sum{'anchor'}, $sum{'link'}, $sum{'line'}, $sum{'size'};
 
