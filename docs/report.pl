fa8745105940e5802feeb8c05246e196bb161b06
Herbert Breunung <lichtkind@cpan.org>
2012-05-04 19:54:40 +0200

    report also heading number

--- docs/report.pl
+++ docs/report.pl
@@ -5,7 +5,8 @@ use v5.10;
 
 my (%file, %data, %sum, $FH, $content, @fn_parts);
 my %pattern = (
-	item   => qr/\n\n<a id|\n### |\w\n\-+\n\n|\w\n=+\n\n/,
+	head   => qr/\n### |\n\n.*\n\-+\n\n|\S\n=+\n\n|\n\n# \[.\]\(#nav-top\)\n\n/,
+	item   => qr/\n\n<a id/,
 	anchor => qr/<a id="/,
 	link   => qr/\]\(/,
 	line   => qr/\n/,
@@ -20,7 +21,7 @@ for my $md_file (<*.txt>){
 
 
 # and sniff through them
-print "    page      item  anchor  links   lines    bytes \n", '-' x 50,"\n";
+print "    page     title   item  anchor  links   lines    bytes \n", '-' x 57,"\n";
 for my $page (sort keys %file) {
 	open $FH, '<', $file{$page};
 	$content = do {local $/; <$FH>};
@@ -30,10 +31,17 @@ for my $page (sort keys %file) {
 		$sum{$type} += $data{$type};
 	}
 	$sum{'size'} += $data{'size'};
-	printf "%11s   %4d   %4d   %5d   %5d   %6.d\n", 
-		$page, $data{'item'}, $data{'anchor'}, $data{'link'}, $data{'line'}, $data{'size'};
+	printf "%11s    %3d   %4d   %4d   %5d   %5d   %6.d\n", 
+           $page, $data{'head'}, $data{'item'}, $data{'anchor'}, $data{'link'},
+                  $data{'line'}, $data{'size'};
 }
-print '-' x 50,"\n";
-printf "        Sum   %4d   %4d   %5d   %5d   %6d\n",
-	$sum{'item'}, $sum{'anchor'}, $sum{'link'}, $sum{'line'}, $sum{'size'};
+print '-' x 57,"\n";
+printf "        Sum    %3d   %4d   %4d   %5d   %5d   %6d\n",
+	$sum{'head'}, $sum{'item'}, $sum{'anchor'},  $sum{'link'},
+	$sum{'line'}, $sum{'size'};
 
+
+# Tester
+#	open $FH, '<', 'tablet-0-history.txt';
+#	$content = do {local $/; <$FH>};
+#	print $content =~ /\S\n=+\n\n/g;
